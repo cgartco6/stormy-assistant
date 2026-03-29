c3a08cf2b92d0c71468df04f2d5765a532bd4323
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-03-30 01:59:33 +0200

    Add service worker for caching strategy

---
+++ public/sw.js
@@ -0,0 +1,7 @@
+const CACHE_NAME = 'stormy-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
